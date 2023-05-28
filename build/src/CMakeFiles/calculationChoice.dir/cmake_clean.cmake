f20af394fbbb15f71b834fcaa2044873f4083306
ritesh006 <ritesh.kumar0793@gmail.com>
2023-05-28 11:28:19 +0530

    confusion here so I am making a commit and taking another branch from start

---
+++ build/src/CMakeFiles/calculationChoice.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculationChoice.dir/calculationChoice.c.o"
+  "CMakeFiles/calculationChoice.dir/calculationChoice.c.o.d"
+  "libcalculationChoice.a"
+  "libcalculationChoice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calculationChoice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/calculationChoice.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for calculationChoice.
+# This may be replaced when dependencies are built.
